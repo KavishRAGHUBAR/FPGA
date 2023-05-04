48477d84be8fa312d659cfbe6d7abb8802cb12ce
Kavish RAGHUBAR <jayesh.raghubar@gmail.com>
2023-05-04 20:28:08 +0200

    DCC bit et et leur TestBench

--- DCC/DCC_BIT_0.vhd
+++ DCC/DCC_BIT_0.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Sorbonne University
+-- Engineer: Raghubar et Dauvet
 -- 
 -- Create Date: 06.03.2023 13:37:04
 -- Design Name: 
@@ -25,7 +25,7 @@ use ieee.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -38,7 +38,7 @@ entity DCC_BIT_0 is
            Reset : in STD_LOGIC;-- Reset Asynchrone
            GO_0 : in STD_LOGIC; --commnde go
            FIN_0 : out STD_LOGIC;--commande fin
-           DCC_0 : out STD_LOGIC);
+           DCC_0 : out STD_LOGIC); --ce qui va représenter bit 0
 end DCC_BIT_0;
 
 architecture Behavioral of DCC_BIT_0 is
@@ -46,23 +46,18 @@ signal start, done_low, done_high : std_logic;
 signal cpt    : std_logic_vector(6 downto 0);	--maintient à  '0' ou à '1' 
 --calcul du delay '0' puis '1' qu'on utilisera dans un process sensible à 1us
 
---on pourra utiliser un compteur delay pour la durée à 1 et la durée à 0
+--on pourra utiliser un compteur delay pour la durée à 0 et la durée à 1
 --on implémentera une mini MAE afin de prendre en compte tous les cas possibles
 
 --MAE : type enuméré
-type etat is (INIT, E_BAS, E_HAUT);
+type etat is (INIT, E_BAS, E_HAUT); --init on ne fait rien, E_Bas -> DCC bit à 0, E_Haut -> DCC bit à 1
 signal EP, EF : etat;
  
 begin
-
 process (Clk_100M, Reset)
 begin
     if (Reset = '1') then
-        cpt <= "0000000";
         EP <= INIT;
-        start <= '0';
-        done_low <= '0';
-        done_high <= '0';
     elsif (rising_edge(Clk_1M) ) then
         EP <= EF;
 --        cpt <= cpt +1;
@@ -76,7 +71,6 @@ begin
     end if;
 end process;
 
-
 process(EP, GO_0, done_low, done_high)
 begin
     case(EP) is
@@ -87,18 +81,25 @@ begin
 end process;
 
 --Sorties en fonction de l'état présent
-process(EP)
-begin
-    case (EP) is 
-        when INIT => DCC_0 <= '0'; start <= '0';
-        when E_BAS => DCC_0 <= '0'; start <= '1';
-        when E_HAUT => DCC_0 <= '1'; start <= '1';
-    end case;
-end process;
+--process(EP,Clk_100M)
+--begin
+--    case (EP) is 
+--        when INIT => DCC_0 <= '0'; start <= '0';
+--        when E_BAS => DCC_0 <= '0'; start <= '1';
+--        when E_HAUT => DCC_0 <= '1'; start <= '1';
+--    end case;
+--end process;
+--en concurrent ça marche mieux
+DCC_0 <= '0' when EP = INIT else
+         '0' when EP = E_BAS else
+         '1' when EP = E_HAUT else '0';
+start <= '0' when EP = INIT else
+         '1'; -- dans les autre états on veut que start soit à 1 afin  d'incrémenter cpt
 
 --process pour le compteur de 58 µs
 process (Clk_1M, start, cpt)
 begin
+   -- if GO_0 = '1' then start
     if start = '0' then 
         cpt <= "0000000"; 
         done_low <= '0';
@@ -117,6 +118,5 @@ begin
      FIN_0 <= '1'; -- on est arrivé à la fin du cycle
      end if;
 end process;
-
 -- make a test bench to check the duration of a bit 0 transmission
 end Behavioral;
--- DCC/DCC_BIT_1.vhd
+++ DCC/DCC_BIT_1.vhd
@@ -1,10 +1,10 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Sorbonne University
+-- Engineer: Raghubar et Dauvet
 -- 
--- Create Date: 06.03.2023 13:39:21
+-- Create Date: 06.03.2023 13:37:04
 -- Design Name: 
--- Module Name: DCC_BIT_1 - Behavioral
+-- Module Name: DCC_bit_1 - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,10 +21,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,17 +33,90 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity DCC_BIT_1 is
-    Port ( Clk : in std_logic; --horloge 1M
+    Port ( Clk_1M : in std_logic; --horloge 1M
            Clk_100M : in std_logic; --horloge 100 M
            Reset : in STD_LOGIC;-- Reset Asynchrone
-           GO_1 : in STD_LOGIC;
-           FIN_1 : out STD_LOGIC;
-           DCC_1 : out STD_LOGIC);
+           GO_1 : in STD_LOGIC; --commnde go
+           FIN_1 : out STD_LOGIC;--commande fin
+           DCC_1 : out STD_LOGIC); --ce qui va représenter bit 0
 end DCC_BIT_1;
 
 architecture Behavioral of DCC_BIT_1 is
-signal delay_1 : std_logic;
+signal start, done_low, done_high : std_logic;
+signal cpt    : std_logic_vector(6 downto 0);	--maintient à  '0' ou à '1' 
+--calcul du delay '0' puis '1' qu'on utilisera dans un process sensible à 1us
+
+--on pourra utiliser un compteur delay pour la durée à 0 et la durée à 1
+--on implémentera une mini MAE afin de prendre en compte tous les cas possibles
+
+--MAE : type enuméré
+type etat is (INIT, E_BAS, E_HAUT); --init on ne fait rien, E_Bas -> DCC bit à 0, E_Haut -> DCC bit à 1
+signal EP, EF : etat;
+ 
 begin
+process (Clk_100M, Reset)
+begin
+    if (Reset = '1') then
+        EP <= INIT;
+    elsif (rising_edge(Clk_1M) ) then
+        EP <= EF;
+--        cpt <= cpt +1;
+--        if (cpt = 100) then
+--        cpt <= 0;
+--            if (dcc_sig = '1') then 
+--                FIN_1 <= '1';
+--            end if;
+--        dcc_sig <= not(dcc_sig);
+--        end if;
+    end if;
+end process;
 
+process(EP, GO_1, done_low, done_high)
+begin
+    case(EP) is
+        when INIT => EF <= INIT; if GO_1 = '1' then EF <= E_BAS; end if;
+        when E_BAS => EF <= E_BAS; if done_low = '1' then EF <= E_HAUT; end if;
+        When E_HAUT => EF <= E_HAUT; if done_high = '1' then EF <= INIT; end if;
+        end case;
+end process;
 
+--Sorties en fonction de l'état présent
+--process(EP,Clk_100M)
+--begin
+--    case (EP) is 
+--        when INIT => DCC_1 <= '0'; start <= '0';
+--        when E_BAS => DCC_1 <= '0'; start <= '1';
+--        when E_HAUT => DCC_1 <= '1'; start <= '1';
+--    end case;
+--end process;
+--en concurrent ça marche mieux
+DCC_1 <= '0' when EP = INIT else
+         '0' when EP = E_BAS else
+         '1' when EP = E_HAUT else '0';
+start <= '0' when EP = INIT else
+         '1'; -- dans les autre états on veut que start soit à 1 afin  d'incrémenter cpt
+
+--process pour le compteur de 58 µs
+process (Clk_1M, start, cpt)
+begin
+   -- if GO_1 = '1' then start
+    if start = '0' then 
+        cpt <= "0000000"; 
+        done_low <= '0';
+        done_high <= '0';
+        FIN_1 <= '0';
+     elsif (rising_edge(Clk_1M) and start = '1') then
+        cpt <= cpt + '1';
+     end if;   
+     
+     if cpt = "0011101" then 
+     done_low <= '1';
+     end if;
+     
+     if cpt = "111010" then 
+     done_high <= '1';
+     FIN_1 <= '1'; -- on est arrivé à la fin du cycle
+     end if;
+end process;
+-- make a test bench to check the duration of a bit 0 transmission
 end Behavioral;
